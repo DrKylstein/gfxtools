56d6bb475d09ba7746dd42810dd7328740111879
Kyle Delaney <dr.kylstein@gmail.com>
2016-06-19 17:35:40 -0400

    Options for planar images with transparency, noted useful 3rd-party tools.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ gfxtools
 ========
 
 Unix-style graphics processing for retro development
+
+
+Some other helpful tools:
+xxd - dump hex or convert hex to raw binary (replaces byteecho.py)
+pngqaunt / pngnq - convert full color images to a limited palette
--- planarbitmap.py
+++ planarbitmap.py
@@ -11,6 +11,10 @@ if __name__ == '__main__':
         help='Number of bit planes to output.')
     parser.add_argument('-i', '--interleave', type=int, default=0, help=
         'Switch planes after this many bytes. The default is contiguous planes.')
+    parser.add_argument('-s', '--solid', action='store_true', help=
+        'Produce a mask plane, but fill it with 1s')
+    parser.add_argument('--ega', action='store_true', help=
+        'Force default EGA palette')
         
     args = parser.parse_args()
         
@@ -27,16 +31,39 @@ if __name__ == '__main__':
     plane_size = image.size[0] * image.size[1] // 8
         
     data = bytearray()
-        
-    for p in range(args.planes):
+    
+    masked = 'transparency' in image.info
+    transparent_color = 0
+    color_planes = args.planes
+    colors = dict(zip(range(1 << args.planes),range(1 << args.planes)))
+    if masked and not args.solid:
+        color_planes -= 1
+        try:
+            transparent_color = image.info['transparency'][0]
+        except TypeError:
+            transparent_color = image.info['transparency']
+        for key,item in colors.items():
+            if item > transparent_color:
+                colors[key] -= 1
+
+    if masked:
         for y in range(image.size[1]):
             for b in range(image.size[0]//8):
                 octet = 0;
                 for x in range(8):
-                    if image.getpixel((b*8 + x,y)) & (1 << p):
-                        octet = octet + (0x80 >> x);
-                data.append(octet)#file.write(bytes([octet]))
-
+                    if args.solid or image.getpixel((b*8 + x,y)) != transparent_color:
+                        octet = octet + (0x80 >> x)
+                data.append(octet)
+        
+    for p in range(color_planes):
+        for y in range(image.size[1]):
+            for b in range(image.size[0]//8):
+                octet = 0;
+                for x in range(8):
+                    if colors[image.getpixel((b*8 + x,y))] & (1 << p):
+                        octet = octet + (0x80 >> x)
+                data.append(octet)
+                
     if args.interleave > 0:
         offsets = [i*plane_size for i in range(args.planes)]
         for contiguous_section in range(plane_size // args.interleave):
